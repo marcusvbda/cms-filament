b03613f0f9d2c9ddb9987cd8c71534dd4fb9584e
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-05-14 10:12:48 -0300

    notifications

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,6 +46,8 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->id('admin')
             ->default()
+            ->databaseNotifications()
+            ->databaseNotificationsPolling('30s')
             ->path($tableParamsExists ? (@Parameter::find('admin_route')?->value ?? "admin") : "admin")
             ->spa()
             ->globalSearchKeyBindings(['command+k', 'ctrl+k'])
