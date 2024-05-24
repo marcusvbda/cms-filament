387d06c5296d1bf192fdfed3e25c4dc0a8a15095
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-05-23 23:11:50 -0300

    resolve urls

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.env') === 'production') {
+            $url = config("app.url");
             URL::forceScheme('https');
         }
+
+        $url = config("app.url");
+        URL::forceRootUrl($url);
 
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
