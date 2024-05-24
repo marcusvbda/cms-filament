5c8a71c5ebcf07d91e2a07ac82e14ec046516509
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-05-24 00:23:21 -0300

    CORRIGE PROXY

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\SetRealHost::class,
     ];
 
     /**
@@ -37,7 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\LanguageMiddleware::class
+            \App\Http\Middleware\LanguageMiddleware::class,
         ],
 
         'api' => [
