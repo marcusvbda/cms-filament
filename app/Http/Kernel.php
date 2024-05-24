9710efbd75e171856a7c7f02c32c4e469eaf4e7d
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-05-24 00:31:06 -0300

    corrige middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,11 +14,10 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        \App\Http\Middleware\TrustHosts::class,
-        \App\Http\Middleware\TrustProxies::class,
-        \jdavidbakr\CloudfrontProxies\CloudfrontProxies::class,
+        // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\ForceAppUrl::class,
         \App\Http\Middleware\TrustProxies::class,
+        \jdavidbakr\CloudfrontProxies\CloudfrontProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
